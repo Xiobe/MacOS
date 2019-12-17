f28a814e964960e7408af3f172d4c0d9ceadb299
Xiobe <Xiobe@users.noreply.github.com>
2019-12-17 21:57:25 +0100

    Loading .zsh added to comment

--- aliases/.zsh
+++ aliases/.zsh
@@ -1,5 +1,5 @@
 # The default shell on MacOS is zsh and not bash this is why this file is called .zsh instead of .bash_profile.
-# The file is also located in $HOME.
+# The file is also located in $HOME. To load the aliases it is just like in Linux 'source <file>'.
 
 # redeclare ls to work with a timestamp that shows the year too.
 alias ls='ls -lT'
