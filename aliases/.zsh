eac087eccd9fc1fd74ff59be3530579297ac9a88
Xiobe <Xiobe@users.noreply.github.com>
2019-12-17 21:55:38 +0100

    Create .zsh

---
+++ aliases/.zsh
@@ -0,0 +1,5 @@
+# The default shell on MacOS is zsh and not bash this is why this file is called .zsh instead of .bash_profile.
+# The file is also located in $HOME.
+
+# redeclare ls to work with a timestamp that shows the year too.
+alias ls='ls -lT'
